e65cfd08238c983383f09b05daaf1df8a4813441
stevsbor <stevensbor@gmail.com>
2024-08-19 12:44:11 +0200

    Removed "database.sql", useless

--- database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS entries (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('Pending', 'Approved', 'Denied') DEFAULT 'Pending'
-);
